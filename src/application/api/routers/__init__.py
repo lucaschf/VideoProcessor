9367a20f06dd086093ac1043ec5ef000879e5fa9
lucaschf <lucaschfonseca@gmail.com>
2025-01-25 14:20:33 -0300

    fix: imports

--- src/application/api/routers/__init__.py
+++ src/application/api/routers/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from .health_check_route import router as health_check_router
+from .user import user_router
 
 
 def register_routes(app: FastAPI) -> None:
@@ -10,6 +11,7 @@ def register_routes(app: FastAPI) -> None:
         app: The FastAPI application instance.
     """
     app.include_router(health_check_router)
+    app.include_router(user_router)
 
 
 __all__ = ["register_routes"]
--- src/domain/video/video_entity.py
+++ src/domain/video/video_entity.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 from src.domain.__shared.entity import Entity
 from src.domain.__shared.validator import ValidationResult
-from src.domain.user.user_entity import User
+from src.domain.user import User
 from src.domain.video.video_entity_validator import VideoValidatorFactory
 
 
